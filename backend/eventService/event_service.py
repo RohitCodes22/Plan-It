744eeeb8826c353b288cd7e7349eb0c4fc03a94b
sean <seantheprofessional1@gmail.com>
2025-11-17 18:26:03 -0600

    some filler code

--- backend/eventService/event_service.py
+++ backend/eventService/event_service.py
@@ -1,18 +1,32 @@
+import databaseService
 
 def create_event(name: str, tags: list, description=""):
     event_obj = {
-        id: 0, # TODO: get this later
-        name: name,
-        tags: tags,
-        description: description,
-        attendees: [],
-        comments: []
+        "id": 0, # TODO: get this later
+        "name": name,
+        "tags": tags,
+        "description": description,
+        "attendees": [],
+        "comments": []
     }
 
-    # TODO: modify the databse
+    databaseService.write_to_db(event_obj)
 
-def update_event(field: str, value: any):
-    pass
 
-def delete_event():
+def update_event(user_id: int, field: str, value: any):
+    TABLE = "event"
+    table = databaseService.retrieve_table(TABLE)
+
+    item = None
+    for i in range(len(table)):
+        if table[i]["id"] == user_id:
+            item = table[i]
+            break
+    
+    item[field] = value
+
+    databaseService.write_to_db()
+   
+            
+def delete_event(id: int):
     pass
--- backend/notification_service/notification_service.py
+++ backend/notification_service/notification_service.py
@@ -1,3 +1,9 @@
-class NotificationService:
-    # TODO: build notification service
+def send_email(email: str):
     pass
+
+def send_notification(user_id: int):
+    # TODO: get email from user service
+    user_email = None
+
+    send_email(user_email)
+    
