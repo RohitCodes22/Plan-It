8803c73d5de562c686f8519b5f8b0f1c8bdf5043
sean <seantheprofessional1@gmail.com>
2025-11-21 05:26:50 -0600

    generating emails more programmatically

--- backend/notification_service/emailer.py
+++ backend/notification_service/emailer.py
@@ -18,15 +18,63 @@ Rohit's very epic birthday party! (5.1 mi.)
 """
 
 
-def send_email(receiver_email: str, message: str) -> None:
-    context = ssl.create_default_context()
+def send_email(message: object) -> int:
+    try:
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(SMTP_SERVER, PORT, context=context) as server:
+            server.login(EMAIL_ADDRESS, PASSWORD)
+            server.send_message(message)
+        return 0
+    except:
+        print("SOMETHING WENT WRONG WHEN SENDING YOUR EMAIL!")
+        print(f"Destination: {receiver_email}")
+        print(f"Contents: {message}")
 
-    with smtplib.SMTP_SSL(SMTP_SERVER, PORT, context=context) as server:
-        print(f"username: {EMAIL_ADDRESS}, password: {PASSWORD}")
-        server.login(EMAIL_ADDRESS, PASSWORD)
-        server.sendmail(EMAIL_ADDRESS, receiver_email, message)
+        return -1
+
+def generate_subscribed_event_email(event_data: list, user_id: int) -> object:
+    """
+    Given an event's relevant data, create an email informing a subscribed user
+    that the event will be re-occurring 
+
+    event_data: {
+        name: str,
+        description: str,
+        date: str
+
+    }
+    """
+    # get user's real name and email
+    user_name = "Sean"
+    user_email = "swy62@umsystem.edu"
+
+    # populate this with actual event data
+    body = f"""\
+    <html>
+        <body>
+            <p>Hi {user_name},<br><br>
+            
+            Did you know some events you subscribed to are happening soon?<br>
+            Here's a quick rundown:
+            </p>
+
+            <ol>
+                <li>
+                    <p>Rohit's database reveal (12/21/2025)</p>
+                </li>
+            </ol>
+        </body>
+    </html>
+
+    """
+    msg = MIMEText(body, "HTML")
+    msg["Subject"] = f"{user_name}, subscribed events happening soon!"
+    msg["From"] = EMAIL_ADDRESS
+    msg["To"] = user_email
+
+    return msg
 
 if __name__ == "__main__":
 
-    send_email("swy62@umsystem.edu", test_message)
+    send_email(generate_subscribed_event_email([], 0))
         
