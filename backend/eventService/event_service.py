2aa04262a873569602ee67203e77b3b717121f21
sean <seantheprofessional1@gmail.com>
2025-11-21 03:49:57 -0600

    creating outline for event_feed  logic

--- backend/eventService/event_service.py
+++ backend/eventService/event_service.py
@@ -30,3 +30,19 @@ def update_event(user_id: int, field: str, value: any):
             
 def delete_event(id: int):
     pass
+
+def generate_event_feed(user_locaton: tuple, filters: set, max_distance: float, num_events = -1) -> list:
+    """
+    Generates a list of local events based on a user location and a set of 
+    inclusive filters
+
+    user_location: (latitude: float, longitude: float),
+    filters: set of string filters based on event-coordinator-defined tags
+    max_distance: float. Defined in miles
+    num_events: int. If -1 then grab all applicable events
+    """
+
+    pass
+
+    # Write a MongoDB query to do this searching efficiently. I think MongoDB 
+    # has built-in support for filtering based on distance, so should be easy
