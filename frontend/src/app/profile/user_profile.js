ebac97ce930cf78e33ea01ee32ea15fc814888b8
Rohit <rsbby@umsystem.edu>
2025-11-30 17:30:32 -0600

    Added backend code for sign up and user profile

--- frontend/src/app/profile/user_profile.js
+++ frontend/src/app/profile/user_profile.js
@@ -1,21 +1,27 @@
-"use client";
-import { useState } from "react";
 import { API_URL } from "../api";
-import { useRouter } from "next/navigation";
 import styles from "./profile.module.css"
 
-export default function ProfilePage() {
+export default async function ProfilePage() {
+    const data = await fetch(`${API_URL}/get_user_info`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include", // VERY IMPORTANT for Flask session cookies
+    }       
+    )
+    const dataForProfile = data.json()
     return (
         <div>
             <h1 className = {styles.title}>User Profile</h1>
             <div>
-                <p1>Name: </p1>
+                <p1>Name: {dataForProfile.fname} {dataForProfile.lname}</p1>
             </div>
             <div>
                 <p1>Username: </p1>
             </div>
             <div>
-                <p1>Email: </p1>
+                <p1>Email: {dataForProfile.email}</p1>
             </div>
             
 
