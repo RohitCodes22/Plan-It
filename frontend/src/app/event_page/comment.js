8fe77b6be1c3bc7638915630456536d32c91439b
sean <seantheprofessional1@gmail.com>
2025-12-04 20:01:58 -0600

    basic event page

---
+++ frontend/src/app/event_page/comment.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function CommentWidget(args) {
+  return (
+    <div className="border-1 rounded-md">
+        <div className="flex flex-row">
+            <div className="h-8 w-8 rounded-full bg-blue-300 m-2 mr-4"></div>
+            <div className="mt-2"> {args.username} </div>
+        </div>
+        
+        <div className="m-2">{args.text}</div>
+    </div>
+  );
+}
