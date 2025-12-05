1be9d2c1885c0614184eda95b162671503fd8b2a
sean <seantheprofessional1@gmail.com>
2025-12-04 20:15:10 -0600

    fixed weird tailwind issue

--- frontend/src/app/event_page/tag_widget.js
+++ frontend/src/app/event_page/tag_widget.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+// had to add this because if I don't, Tailwind will purge unused class names
+const colorMap = {
+    green: "text-green-600 bg-green-100",
+    blue: "text-blue-600 bg-blue-100",
+    red: "text-red-600 bg-red-100",
+    purple: "text-purple-600 bg-purple-100",
+    yellow: "text-yellow-600 bg-yellow-100",
+};
+
 export default function TagWidget(args) {
-  let cssString = `rounded border-1 border-grey-100 pl-1 pr-1 text-${args.color}-700 bg-${args.color}-200 font-bold hover:scale-102`
-  console.log(cssString)
   return (
-    <li className={cssString}>
+    <li className={`transition-all select-none rounded border-1 border-grey-100 pl-1 pr-1 font-bold hover:scale-106 ${colorMap[args.color]}`}>
         {args.text}
     </li>
   );
