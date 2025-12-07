6eed8c45b9ddb884eae22727233d54f1e54b5acb
sean <seantheprofessional1@gmail.com>
2025-12-07 11:31:08 -0600

    populating tags

--- frontend/src/app/event_page/event_widget.js
+++ frontend/src/app/event_page/event_widget.js
@@ -5,44 +5,57 @@ import CommentWidget from "./comment";
 import CommentInput from "./comment_input";
 
 export default function EventWidget(args) {
-  const router = useRouter();
+    const router = useRouter();
 
-  return (
-    <div className="w-full flex flex-col gap-6">
-      
-      <header className="text-left">
-        <h1 className="text-3xl font-bold">{args.data.name}</h1>
-      </header>
+    function tagStringToList(itemsStr) {
+        return (itemsStr.slice(1, itemsStr.length - 1).replaceAll('"', '')).split(',');
+    }
 
-      <div className="flex flex-col gap-3">
-        <div className="rounded-2xl pl-3 pt-1 pb-1 pr-3 border-1 border-grey-100 w-fit"> Rohit Sayeeganesh</div>
-        <div>
-            <h3 className="text-bold text-xl">Tags</h3>
+    function TagList(items) {
+        const colorList = ["blue", "green", "red", "orange", "purple", "yellow"];
+        return (
             <ol className="flex flex-row gap-2 ml-2 mt-2"> 
-                <TagWidget text="fun" color="blue"/> 
-                <TagWidget text="study" color="green"/> 
+                {items.map((item, index) => {
+                    return <TagWidget text={item} color={colorList[index % colorList.length]}/> 
+                })}
             </ol>
+        );
+    }
 
-            <h3 className="text-bold text-xl mt-4 font-semibold">Description</h3>
-            <hr/>
-            <div className="ml-2">
-                <p>{args.data.description},</p>
+
+    return (
+        <div className="w-full flex flex-col gap-6">
+        
+        <header className="text-left">
+            <h1 className="text-3xl font-bold">{args.data.name}</h1>
+        </header>
+
+        <div className="flex flex-col gap-3">
+            <div className="rounded-2xl pl-3 pt-1 pb-1 pr-3 border-1 border-grey-100 w-fit"> Rohit Sayeeganesh</div>
+            <div>
+                <h3 className="text-bold text-xl">Tags</h3>
+                {TagList(tagStringToList(args.data.tags))}
+
+                <h3 className="text-bold text-xl mt-4 font-semibold">Description</h3>
+                <hr/>
+                <div className="ml-2">
+                    <p>{args.data.description},</p>
+                </div>
+            </div>
+
+            <div>
+                <h2 className="text-2xl font-semibold"> Comments </h2>
+                <hr/>
+                <div className="mt-3">
+                    <CommentInput/>
+                    <CommentWidget
+                    username="SpiderHit"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
+                    />
+                </div>
             </div>
         </div>
 
-        <div>
-            <h2 className="text-2xl font-semibold"> Comments </h2>
-            <hr/>
-            <div className="mt-3">
-                <CommentInput/>
-                <CommentWidget
-                  username="SpiderHit"
-                  text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat"
-                />
-            </div>
         </div>
-      </div>
-
-    </div>
-  );
+    );
 }
