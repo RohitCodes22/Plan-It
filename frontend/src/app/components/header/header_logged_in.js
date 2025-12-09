808bc7acf8ba3dcbe711e00954345476e970e891
Rohit <rsbby@umsystem.edu>
2025-12-08 20:45:42 -0600

    Updated header shading

--- frontend/src/app/components/header/header_logged_in.js
+++ frontend/src/app/components/header/header_logged_in.js
@@ -4,7 +4,7 @@ import logo from "../header/Plan.png"
 
 export default function Header() {
   return (
-    <header className="w-full shadow-md py-4 px-6 flex items-center justify-between bg-white text-black">
+    <header className="fixed w-full shadow-md py-4 px-6 flex items-center justify-between bg-white text-black">
       <div className="flex items-center gap-2">
         <Image src={logo} alt="App Logo" width={40} height={40} />
         <span className="text-xl font-bold">Plan-It!</span>
