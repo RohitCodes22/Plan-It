924292cf8eb795a8eb9bbc0825a3066e63681129
Rohit <rsbby@umsystem.edu>
2025-12-08 18:29:41 -0600

    Added logo to logged in navbar as well. forgot to do that

--- frontend/src/app/components/header/header_logged_in.js
+++ frontend/src/app/components/header/header_logged_in.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Image from "next/image";
+import logo from "../header/Plan.png"
 
 export default function Header() {
   return (
     <header className="w-full shadow-md py-4 px-6 flex items-center justify-between bg-white text-black">
-      <div className="flex items-center gap-2 text-xl font-semibold">
-        <span>Plan-IT</span>
+      <div className="flex items-center gap-2">
+        <Image src={logo} alt="App Logo" width={40} height={40} />
+        <span className="text-xl font-bold">Plan-It!</span>
       </div>
 
       <nav className="flex items-center gap-4">
