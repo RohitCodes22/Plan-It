e967fc718e16c24d93b324c7024d8d2d94332388
Colin115 <89537192+Colin115@users.noreply.github.com>
2025-12-13 10:02:26 -0600

    touched up event page

--- frontend/src/app/home/page.jsx
+++ frontend/src/app/home/page.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Header from '../components/header/header_logged_in'
-import BottomNavBar from '../components/bottomNavBar/bottom_nav_bar'
-import EventFeed from '../components/eventFeed/eventFeed'
+import React from "react";
+import Header from "../components/header/header_logged_in";
+import BottomNavBar from "../components/bottomNavBar/bottom_nav_bar";
+import EventFeed from "../components/eventFeed/eventFeed";
 const Page = () => {
   return (
     <div>
-        <Header />
-        <div>
-            <EventFeed />
-        </div>
-        <BottomNavBar />
+      <Header />
+      <main className="flex-1 mb-15">
+        <EventFeed />
+      </main>
+      <BottomNavBar />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
